43e77e1f2da28e96f4a227c3677b011e138ec05a
Darshana Herath <gihankck15@gmail.com>
2021-02-10 20:04:08 +0530

    footer edit

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -1,6 +1,7 @@
 import { CustomerDetailsService } from '../services/customer-reg.service';
 import { UserDetails, SellerDetailsService } from '../services/seller-details.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-my-profile',
@@ -10,14 +11,14 @@ import { Component, OnInit } from '@angular/core';
 export class MyProfileComponent implements OnInit {
 
 
-  constructor(private auth2: CustomerDetailsService,
+  constructor(private router: Router,private auth2: CustomerDetailsService,
     private auth3: SellerDetailsService) { }
 
   ngOnInit(): void {
     
   }
 
-  details = this.auth2.getUserDetails();
+  details = this.auth2.getUserDetails()==null ? this.router.navigate(['signin']):this.auth2.getUserDetails();
   name = this.auth2.getUserDetails().full_name ==undefined?this.auth3.getUserDetails().shopName:this.auth2.getUserDetails().full_name;
   email = this.auth2.getUserDetails().email;
   address = this.auth2.getUserDetails().address;
